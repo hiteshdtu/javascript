4c8f71d7b7c00c5526350d1af2fca3c63aa7591e
hiteshdtu <37405906+hiteshdtu@users.noreply.github.com>
2018-03-27 13:27:06 +0530

    first

---
+++ map.js
@@ -0,0 +1,18 @@
+<html>
+<script>
+<head>
+let map= new Map();
+
+
+map.set('Ronaldo',94);
+
+map.set('Messi',95);
+map.set('Pogba',89);
+map.set('bale',91);
+
+alert(map.size);
+map.delete('bale');
+alert(map.size);
+</head>
+</script>
+</html>
